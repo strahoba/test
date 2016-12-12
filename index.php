9906e7c5fe8a24030ba4afd8f6cdf3a339e5a5c5
Igor Scepanovic <endava.iscepanovic@ad.mobtel.com>
2016-12-12 13:29:15 +0100

    dva nova fajla

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+	/**
+	 * Autor: endava.iscepanovic
+	 * Datum: 9.12.2016
+	 * Vreme: 15:07
+	 * -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+	 * Projekat: test
+	 *
+	 */
+
+
+	$b = pg_escape_string( $_POST[ 'nesto' ] );
+
+	if ( isset( $b ) ) {
+		echo $b;
+	}
+
+
+?>
+
+<p>Click <a href = "">here</a> to continue</p>                                  	
+
+
+
+<div class = "mojaKlasa">
+	<h1>Random naslov bla</h1>
+	<br>
+	<p>random tekst</p>
+</div>
+
+
+
+
+
